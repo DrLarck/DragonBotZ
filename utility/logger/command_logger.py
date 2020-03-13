79da9963f4bb41107bd3f1671b0f3865eea6872b
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-03-13 09:43:47 +0100

    Add .execute statement to log method
    Add the database.execute statement to the log method.

--- utility/logger/command_logger.py
+++ utility/logger/command_logger.py
@@ -33,7 +33,19 @@ class CommandLogger:
 
         # Init
         command_name = context.command.name
-        command_author = context.message.author
+        command_param = context.args
+        command_date = time.strftime("%d/%m/%y - %H:%M", time.gmtime())  # Get the date and the hour
+        command_time = time.time()  # Get the full time as second since epoch
+        command_message = context.message
+        command_author = command_message.author
 
-        
+        # Writing log
+        await self.__database.execute("""INSERT INTO command_log(
+                                      command, parameter, date, 
+                                      time, caller_id, caller_name, message)
+                                      VALUES($1, $2, $3, $4, $5, $6, $7)""",
+                                      [command_name, command_param, command_date,
+                                       command_time, command_author.id, command_author.name,
+                                       command_message])
 
+        return 
