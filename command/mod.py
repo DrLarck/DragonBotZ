adc00b2fa80675dc015f5f93dac3e2320142026b
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-03-13 10:22:27 +0100

    Add parameter to the test command
    Add parameters to the test command to test out the new command logger

--- command/mod.py
+++ command/mod.py
@@ -9,6 +9,7 @@ Last update : 13/03/20 by DrLarck
 """
 
 from discord.ext import commands
+from utility.logger.command_logger import CommandLogger
 
 
 class CommandModeration(commands.Cog):
@@ -19,8 +20,10 @@ class CommandModeration(commands.Cog):
 
     # Command
     @commands.command()
-    async def test(self, context):
-        await context.send("This is a test")
+    async def test(self, context, param1, param2, param3):
+        await context.send(f"This is a test : {param1} {param2} {param3}")
+
+        await CommandLogger().log(context)
 
 
 def setup(client):
