7ea0f329591e12063a5b57aa9193f7ef76407364
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-03-13 11:52:37 +0100

    Add no_dmg check
    The no_dm check avoid the player to use certain commands in DM channel

--- utility/command/checker.py
+++ utility/command/checker.py
@@ -5,9 +5,10 @@ Command checker
 
 Author : DrLarck
 
-Last update : 12/03/20 by DrLarck
+Last update : 13/03/20 by DrLarck
 """
 
+from discord.channel import DMChannel
 
 class CommandChecker:
 
@@ -28,3 +29,26 @@ class CommandChecker:
         client = context.bot
 
         return client.is_ready()
+
+    @staticmethod
+    async def no_dm(context):
+        """
+        Avoid the player to use the command in DM channel
+
+        :param context:
+
+        --
+
+        :return: `bool`
+        """
+
+        # Init
+        message_channel = context.message.channel
+
+        # If the channel is a DM channel
+        if isinstance(message_channel, DMChannel):
+            return False  # The command will be ignored
+
+        # Not a DM channel
+        else:
+            return True
