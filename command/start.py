3de8ffeef5ae9ae9ae3bd937deae08ae0c9fa082
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-03-13 13:37:44 +0100

    Add Start command
    The start command register the player into the database

--- command/start.py
+++ command/start.py
@@ -8,14 +8,54 @@ Author : DrLarck
 Last update : 13/03/20 by DrLarck
 """
 
+import time
+
 from discord.ext import commands
 
-class CommandStart(commands.Cog)
+# util
+from utility.player import Player
+from utility.database import Database
+from utility.command.checker import CommandChecker
+
+
+class CommandStart(commands.Cog):
 
     def __init__(self, client):
         # Public
         self.client = client
 
+        # Private
+        self.__start_dragonstone = 50
+        self.__start_zenis = 100000
+
+    @commands.command()
+    async def start(self, context):
+        """
+        Register the player into the database
+
+        :param context: (`discord.ext.commands.Context`)
+
+        --
+
+        :return: `None`
+        """
+
+        # Init
+        player = Player(context.message.author)
+        date = time.strftime("%d/%m/%y - %H:%M", time.gmtime())
+        database = Database()
+
+        # Register the player into the database
+        await database.execute("""
+                               INSERT INTO player_info(
+                               player_id, player_name,
+                               player_register_date, player_language)
+                               VALUES($1, $2, $3, $4);
+                               """, [player.id, player.name, date, 'EN'])
+
+        return
+
 
 def setup(client):
     client.add_cog(CommandStart(client))
+
