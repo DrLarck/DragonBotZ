ba58187c0a05a716ddb1a9ab02a3ddc5e4806311
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-03-19 12:28:16 +0100

    Add character_reference table | Add CharacterGetter
    Add the character_reference table creation query.
    The CharacterGetter now works.

--- utility/entity/character_getter.py
+++ utility/entity/character_getter.py
@@ -5,11 +5,12 @@ Character getter
 
 Author : DrLarck
 
-Last update : 16/03/20 by DrLarck
+Last update : 19/03/20 by DrLarck
 """
 
 # util
 from utility.database import Database
+from utility.entity.character import Character
 
 
 class CharacterGetter:
@@ -19,17 +20,36 @@ class CharacterGetter:
         self.__database = Database()
 
     # Public
-    async def get_base_character(self, base_id):
+    async def get_reference_character(self, reference):
         """
         Get a base character
 
-        :param base_id: (`int`)
+        :param reference: (`int`)
 
         --
 
-        :return: `Character`
+        :return: `Character` or `None`
         """
 
         # Init
+        character = Character()
 
+        # Get the character from the database
+        data = await self.__database.fetch_row("""
+                                              SELECT * FROM character_reference 
+                                              WHERE reference = $1; 
+                                              """, [reference])
 
+        if len(data) > 0:  # Character found
+            data = data[0]
+
+            # Get a character instance according to the data we just fetched
+            character = await character.generate(char_id=data[0], name=data[1], type_value=data[2],
+                                                 rarity_value=data[3], card=data[4], health=data[5],
+                                                 physical=data[6])
+
+            return character
+
+        else:  # Character not found
+            print(f"Character #{reference} not found.")
+            return
