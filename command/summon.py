ed47c597274e04a618de4aad0035baff548517ef
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-03-20 14:44:00 +0100

    Add summon command

--- command/summon.py
+++ command/summon.py
@@ -10,11 +10,35 @@ Last update : 20/03/20 by DrLarck
 
 from discord.ext import commands
 
+# util
+from utility.command.checker import CommandChecker
+from utility.entity.banner import BannerGetter
+
 
 class CommandSummon(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+
+        # Private
+        self.__cost = 5
+        self.__getter = BannerGetter
+
+    @commands.check(CommandChecker.game_ready)
+    @commands.check(CommandChecker.register)
+    @commands.command()
+    async def summon(self, context, banner_reference: int):
+
+        # Init
+        banner = await self.__getter.get_banner(banner_reference)
+
+        if banner is not None:
+            summoned = await banner.summon()
+
+            await summoned.get_display_card()
+
+        else:
+            await context.send(f"Banner **{banner_reference:,}** not found")
 
 
 def setup(client):
