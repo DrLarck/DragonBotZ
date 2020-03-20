5162e5bb9a7369ef25e44cbf97f066f599b20c81
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-03-20 16:02:45 +0100

    Add resource check

--- command/summon.py
+++ command/summon.py
@@ -13,6 +13,8 @@ from discord.ext import commands
 # util
 from utility.command.checker import CommandChecker
 from utility.entity.banner import BannerGetter
+from utility.entity.player import Player
+from utility.graphic.icon import GameIcon
 
 
 class CommandSummon(commands.Cog):
@@ -30,17 +32,32 @@ class CommandSummon(commands.Cog):
     async def summon(self, context, banner_reference: int = 1):
 
         # Init
+        player = Player(self.client, context.message.author)
+        icon = GameIcon()
         banner = await self.__getter.get_banner(banner_reference)
 
-        if banner is not None:
+        # Check if the player has enough resource to summon
+        player_dargonstone = await player.resource.get_dragonstone()
+
+        # If the player has enough to summon
+        if player_dargonstone >= self.__cost and banner is not None:
             summoned = await banner.summon()
 
             character_display = await summoned.get_display_card(self.client)
 
+            # Remove the amount of stones used
+            await player.resource.remove_dragonstone(self.__cost)
+
+            # Display the card
             await context.send(embed=character_display)
 
-        else:
+        # If the banner has not been found
+        elif banner is None:
             await context.send(f"Banner **{banner_reference:,}** not found")
+
+        # If the player doesn't have enough resources
+        else:
+            await context.send(f"You do not have enough **Dragon stones**{icon.dragonstone} to summon")
 
 
 def setup(client):
