554320fc117009eddaf11e7ce25abeeba0a4586b
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-03-21 00:04:00 +0100

    Add get_latest_banner method
    This method will return the last banner stored in the banner cache

--- utility/entity/banner.py
+++ utility/entity/banner.py
@@ -5,7 +5,7 @@ Banner object
 
 Author : DrLarck
 
-Last update : 20/03/20 by DrLarck
+Last update : 21/03/20 by DrLarck
 """
 
 import asyncio
@@ -338,3 +338,14 @@ class BannerGetter:
         else:
             print(f"Banner {reference} not found.")
             return None
+
+    async def get_latest_banner(self):
+        """
+        Return the last banner stored in the cache
+
+        --
+
+        :return: `Banner`
+        """
+
+        return self.__cache[len(self.__cache) - 1]
