a05d256d0155cb7ab2abeb1ede7395e4ca6728d9
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-03-20 22:51:30 +0100

    Update version to v1.0.0.48

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class Main:
 
     def __init__(self):
         self.__TOKEN = os.environ["dev-dbz-token"]
-        self.__version = "1.0.0.35"
+        self.__version = "1.0.0.48"
         self.__phase = ["ALPHA", "BETA", "RELEASE", "STABLE"]
 
         self.__prefix = ["d!", "D!", "db", "Db"]
