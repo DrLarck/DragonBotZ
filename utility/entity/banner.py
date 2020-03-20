e7a9c9c5779bbee22df9d6895af1d39d1f3f3c73
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-03-20 12:05:32 +0100

    Add Banner object and sort() method

---
+++ utility/entity/banner.py
@@ -0,0 +1,68 @@
+"""
+Banner object
+
+--
+
+Author : DrLarck
+
+Last update : 20/03/20 by DrLarck
+"""
+
+import asyncio
+
+
+class Banner:
+
+    def __init__(self):
+        # Public
+        self.name = ""
+        self.image = ""
+        self.characters = []
+        self.sorted = False
+
+        # Private
+        self.__lr = []
+        self.__ur = []
+        self.__ssr = []
+        self.__sr = []
+        self.__r = []
+        self.__n = []
+
+    # Public
+    async def sort(self):
+        """
+        Sort the banner
+
+        --
+
+        :return: `None`
+        """
+
+        if self.sorted is False:
+            # Sort the characters
+            for character in self.characters:
+                await asyncio.sleep(0)
+
+                # Put the character in the LR list
+                if character.rarity.value == 5:
+                    self.__lr.append(character)
+
+                # Put the character in the UR list
+                elif character.rarity.value == 4:
+                    self.__ur.append(character)
+
+                elif character.rarity.value == 3:
+                    self.__ssr.append(character)
+
+                elif character.rarity.value == 2:
+                    self.__sr.append(character)
+
+                elif character.rarity.value == 1:
+                    self.__r.append(character)
+
+                elif character.rarity.value == 0:
+                    self.__n.append(character)
+
+                self.sorted = True
+
+        return
