cdb89761c7b13382bf8ac20397b1661cadb5d166
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-03-20 13:20:42 +0100

    Add summon method

--- utility/entity/banner.py
+++ utility/entity/banner.py
@@ -9,6 +9,7 @@ Last update : 20/03/20 by DrLarck
 """
 
 import asyncio
+import random
 
 
 class Banner:
@@ -32,8 +33,8 @@ class Banner:
         self.__lr_droprate = 0.01
         self.__ur_droprate = 3
         self.__ssr_droprate = 15
-        self.__ssr_droprate = 33
-        self.__lr_droprate = 50
+        self.__sr_droprate = 33
+        self.__r_droprate = 50
         self.__n_droprate = 100
 
     # Public
@@ -72,5 +73,53 @@ class Banner:
                     self.__n.append(character)
 
                 self.sorted = True
+
+        return
+
+    async def summon(self):
+        """
+        Summon a random character
+
+        --
+
+        :return: `Character` or `None` in case of problem
+        """
+
+        # Init
+        roll = random.uniform(0, 100)
+
+        # Get a character according to the player's roll right above
+        # Check if the list is not empty
+        if len(self.__lr) > 0 and roll <= self.__lr_droprate:
+            # If the player get a LR character
+            # Get a random LR character form the LR list
+            character = random.choice(self.__lr)
+
+            return character
+
+        elif len(self.__ur) > 0 and roll <= self.__ur_droprate:
+            character = random.choice(self.__ur)
+
+            return character
+
+        elif len(self.__ssr) > 0 and roll <= self.__ssr_droprate:
+            character = random.choice(self.__ssr)
+
+            return character
+
+        elif len(self.__sr) > 0 and roll <= self.__sr_droprate:
+            character = random.choice(self.__sr)
+
+            return character
+
+        elif len(self.__r) > 0 and roll <= self.__r_droprate:
+            character = random.choice(self.__r)
+
+            return character
+
+        elif len(self.__n) > 0 and roll <= self.__n_droprate:
+            character = random.choice(self.__n)
+
+            return character
 
         return
