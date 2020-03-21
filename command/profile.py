096d57e839bc3c9c9929bdd0afde428a7cb1530b
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-03-21 18:50:10 +0100

    Add profile command skeleton

--- command/profile.py
+++ command/profile.py
@@ -8,13 +8,44 @@ Author : DrLarck
 Last update : 21/03/20 by DrLarck
 """
 
+import discord
+
 from discord.ext import commands
+
+# util
+from utility.entity.player import Player
+from utility.command.checker import CommandChecker
+from utility.graphic.embed import CustomEmbed
 
 
 class CommandProfile(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+
+    @commands.check(CommandChecker.game_ready)
+    @commands.check(CommandChecker.register)
+    async def profile(self, context, target: discord.Member = None):
+
+        # Log
+        await self.client.logger.log(context)
+
+        # Init
+        # If no target is provided
+        # The player is the message author
+        if target is None:
+            player = Player(self.client, context.message.author)
+
+        # If the target is provided
+        # The player is the target
+        else:
+            player = Player(self.client, target)
+
+        embed = CustomEmbed()
+
+        # Initialize embed's informations
+        level = player.level
+        
 
 
 def setup(client):
