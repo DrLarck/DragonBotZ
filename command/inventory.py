78b419dd62aa4b1f5f0e3bec407dac6f31e3570f
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-04-05 13:49:24 +0100

    Add inventory command

--- command/inventory.py
+++ command/inventory.py
@@ -5,16 +5,33 @@ Inventory command
 
 Author : DrLarck
 
-Last update : 21/03/20 by DrLarck
+Last update : 05/04/20 by DrLarck
 """
 
 from discord.ext import commands
+
+# util
+from utility.entity.player import Player
+
+# tool
+from utility.command.tool.tool_inventory import ToolInventory
 
 
 class CommandInventory(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+
+        # Private
+        self.__tool = ToolInventory(self.client)
+
+    @commands.command()
+    async def inventory(self, context):
+        # Init
+        player = Player(self.client, context.message.author)
+        inventory = await self.__tool.get_inventory_embed(player)
+
+        await context.send(embed=inventory)
 
 
 def setup(client):
