3881ae7d27ec11d7091a71a92252fb159e37b171
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-04-05 13:36:37 +0100

    Add get_inventory_embed method
    Return an embed of the player's inventory

--- utility/command/tool/tool_inventory.py
+++ utility/command/tool/tool_inventory.py
@@ -8,6 +8,48 @@ Author : DrLarck
 Last update : 21/03/20 by DrLarck
 """
 
+# util
+from utility.graphic.embed import CustomEmbed
+
 
 class ToolInventory:
-    pass
+
+    def __init__(self, client):
+        # Public
+        self.client = client
+
+        # Private
+        self.__embed = CustomEmbed()
+        self.__database = self.client.database
+
+    # Public
+    async def get_inventory_embed(self, player):
+        """
+        Return an embed message of the player's inventory
+
+        :param player: (`Player`)
+
+        --
+
+        :return: `discord.Embed`
+        """
+
+        # Init
+        player_characters = await self.__database.fetch_row("""
+                                                            SELECT *
+                                                            FROM character_unique
+                                                            WHERE player_id = $1;
+                                                            """, [player.id])
+
+        collection = len(player_characters)
+        dragonstone = await player.resource.get_dragonstone()
+        zeni = await player.resource.get_zeni()
+
+        # Setup the embed
+        self.__embed = await self.__embed.setup(self.client, title=f"{player.name}'s inventory",
+                                                thumbnail_url=player.avatar)
+
+        # Embed fields
+        self.__embed.add_field(name="")
+
+        return self.__embed
