530c27eb92575b390c28940b2c4ac1a04c6dc14f
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-04-07 17:16:27 +0100

    Add ToolBox object
    The ToolBox object allows you to manage the player's box

---
+++ utility/command/tool/tool_box.py
@@ -0,0 +1,145 @@
+"""
+Box command tools
+
+--
+
+Author : DrLarck
+
+Last update : 07/04/20 by DrLarck
+"""
+
+import asyncio
+
+# util
+from utility.interactive.button import Button
+from utility.graphic.embed import CustomEmbed
+from utility.entity.character import CharacterGetter
+
+
+class ToolBox:
+
+    def __init__(self, client, context):
+        # Public
+        self.client = client
+        self.context = context
+
+        # Private
+        self.__database = self.client.database
+        self.__data = None
+        self.__display_per_page = 5
+        self.__total_page = 0
+        self.__getter = CharacterGetter()
+
+    # Public
+    async def box_manager(self, player):
+        """
+        Manage the box displaying
+
+        :param player: (`Player`)
+
+        --
+
+        :return: `None`
+        """
+
+        # Init
+        if self.__data is None:
+            # Get all the distinct characters from the database
+            self.__data = await self.__database.fetch_row("""
+                                                          SELECT DISTINCT character_reference
+                                                          FROM character_unique
+                                                          WHERE character_owner_id = $1
+                                                          ORDER BY character_reference;
+                                                          """, [player.id])
+
+            # Get the character objects
+            new_data = []
+
+            for reference in self.__data:
+                await asyncio.sleep(0)
+
+                character = self.__getter.get_reference_character(reference)
+
+                new_data.append(character)
+
+            # Replace the data by the characters objects
+            self.__data = new_data
+
+        # If the player has at least one character
+        if len(self.__data) > 0:
+            # Get the total number of page
+            self.__total_page = int(
+                                   (len(self.__data) - 1 / self.__display_per_page) + 1
+                                   )
+
+            # Display and manage the box behaviour
+            stop = False
+
+            while not stop:
+                # Display the box page from the page 1
+                box_page = await self.get_box(player, 1)
+
+                await self.context.send(embed=box_page)
+                break
+
+        # If the player doesn't have any character
+        else:
+            await self.context("You do not have any character")
+
+        return
+
+    async def get_box(self, player, page):
+        """
+        Get the box page embed
+
+        :param player: (`Player`)
+        :param page: (`int`)
+
+        --
+
+        :return: `discord.Embed`
+        """
+
+        # Init
+        box_page = await CustomEmbed().setup(self.client,
+                                             title=f"{player.name}'s box",
+                                             description=f"Page {page}/{self.__total_page}",
+                                             thumbnail_url=player.avatar)
+
+        # Display the characters
+        # Get the first character to display
+        # For example, if you are at page 1
+        # The first character's index would be 0, and the last one 4, it would display 5 characters
+        # according to the display_per_page attribute which is editable
+        # If you are at the page 2, the first character index to display would be 5 and the last one 9
+        # and so on ...
+        start = (page - 1) * self.__display_per_page
+
+        end = page * self.__display_per_page
+
+        # Add characters to the embed
+        characters = ""
+
+        for i in range(start, end):
+            await asyncio.sleep(0)
+
+            # Get the current character
+            character = self.__data[i]
+
+            # Get the amount of characters the player owns
+            amount = await self.__database.fetch_row("""
+                                                     SELECT * FROM character_unique
+                                                     WHERE character_reference = $1
+                                                     AND character_owner_id = $2;
+                                                     """, [character.id, player.id])
+
+            amount = len(amount)
+
+            # Display the character
+            characters += f"**{character.name}**{character.rarity.icon} x{amount}"
+
+        box_page.add_field(name="Characters",
+                           value=characters,
+                           inline=False)
+
+        return box_page
