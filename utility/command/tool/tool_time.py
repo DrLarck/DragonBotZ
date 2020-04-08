b7a1217e4947f98cde8e799d8fb0d4b70ce6ce91
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-04-08 15:45:25 +0100

    Add Time tool object

---
+++ utility/command/tool/tool_time.py
@@ -0,0 +1,77 @@
+"""
+Time tool object
+
+--
+
+Author : DrLarck
+
+Last update : 08/04/20 by DrLarck
+"""
+
+
+class ToolTime:
+
+    @staticmethod
+    async def convert_time(seconds):
+        """
+        Convert the passed seconds into a time string of format :
+
+        - 00d 00h 00m 00s
+
+        OR
+
+        - 00h 00m 00s
+
+        OR
+
+        - 00m 00s
+
+        OR
+
+        - 00s
+
+        :param seconds: (`int`)
+
+        --
+
+        :return: `str`
+        """
+
+        # Init
+        # Time references (in seconds)
+        ref_day = 86400
+        ref_hour = 3600
+        ref_minute = 60
+        ref_second = 1
+        time_string = ""
+
+        # Calculate the day
+        day = seconds // ref_day
+        seconds -= ref_day * day
+
+        # Calculate the hour
+        hour = seconds // ref_hour
+        seconds -= ref_hour * hour
+
+        # Calculate the minute
+        minute = seconds // ref_minute
+        seconds -= ref_minute * minute
+
+        # Calculate the second
+        second = seconds // ref_second
+        seconds -= ref_second * second
+
+        # Setup the string
+        if day > 0:
+            time_string += f"{day:,}d "
+
+        if hour > 0:
+            time_string += f"{hour:,}h "
+
+        if minute > 0:
+            time_string += f"{minute:,}m "
+
+        if second > 0:
+            time_string += f"{second:,}s"
+
+        return time_string
