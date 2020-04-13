f0c8c7d6f2d120b2a8e34f3c11bd99b682f6d432
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-04-13 16:50:00 +0100

    Add Z-Sword lv.1 skeleton

---
+++ utility/entity/item/training_item/sword_0.py
@@ -0,0 +1,31 @@
+"""
+Represent lv.1 Z-Sword
+
+--
+
+Author : DrLarck
+
+Last update : 13/04/20 by DrLarck
+"""
+
+# util
+from utility.entity.training_item import TrainingItem
+from utility.graphic.icon import GameIcon
+
+
+class Sword0(TrainingItem):
+
+    def __init__(self, client, character):
+        """
+        :param client: (`discord.ext.commands.Bot`)
+
+        :param character: (`Character`)
+        """
+
+        # Inheritance
+        TrainingItem.__init__(self, client, character)
+
+        # Public
+        self.reference = 0
+        self.name = "Z-Sword lv.1"
+        self.icon = GameIcon().sword_0
