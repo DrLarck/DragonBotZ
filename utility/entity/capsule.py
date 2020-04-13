39aa822422a610b09aca2fb005b568a09cb366e0
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-04-13 16:41:15 +0100

    Add item on capsule open

--- utility/entity/capsule.py
+++ utility/entity/capsule.py
@@ -7,6 +7,10 @@ Author : DrLarck
 Last update : 13/04/20 by DrLarck"""
 
 import random
+import asyncio
+
+# tool
+from utility.global_tool import GlobalTool
 
 
 class Capsule:
@@ -26,8 +30,12 @@ class Capsule:
         # Info
         self.name = ""
         self.icon = ""
+        self.reference = 0
 
         # Private
+        self.__database = self.client.database
+        self.__global_tool = GlobalTool()
+
         # Rewards
         self.__dragonstone = 0
         self.__zeni = 0
@@ -58,5 +66,51 @@ class Capsule:
             # Check if the player has loot a training item
             if roll <= self.__rate_item:
                 item = random.choice(self.__item)
-                
+
+                # Add the item into the database
+                await self.__database.execute("""
+                                              INSERT INTO training_item(training_item_reference, owner_id, owner_name)
+                                              VALUES($1, $2, $3);
+                                              """, [item.reference, self.player.id, self.player.name])
+
+                # Generate a unique id for the item
+                await item.set_unique_id()
+
+                # Display the message
+                msg = f"You've opened a {self.icon}**{self.name}** capsule and found {item.icon}**{item.name}** in it"
+                await self.context.send(msg)
+
+        return
+
+    async def set_unique_id(self):
+        """
+        Generate an unique id for the capsule that have 'NONE' as unique id
+
+        --
+
+        :return: `None`
+        """
+
+        # Get the capsule that have 'NONE' as unique id
+        capsules = await self.__database.fetch_row("""
+                                                   SELECT reference
+                                                   FROM capsule
+                                                   WHERE capsule_unique_id = 'NONE';
+                                                   """)
+
+        # Generate an unique id for each of those capsules
+        for capsule in capsules:
+            await asyncio.sleep(0)
+
+            # Get the capsule reference
+            reference = capsule[0]
+            unique_id = await self.__global_tool.generate_unique_id(reference)
+
+            # Update the id
+            await self.__database.execute("""
+                                          UPDATE capsule
+                                          SET unique_id = $1
+                                          WHERE reference = $2;
+                                          """, [unique_id, reference])
+
         return
