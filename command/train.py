dcc27e68c0701a37ccf2a976509c326a16c21f71
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-08-03 16:10:14 +0100

    Add a way for characters to gain exp

--- command/train.py
+++ command/train.py
@@ -4,7 +4,10 @@
 
 @author DrLarck
 
-@update 30/07/20 by DrLarck"""
+@update 03/08/20 by DrLarck"""
+
+import asyncio
+import random
 
 from discord.ext import commands
 
@@ -15,6 +18,7 @@ from utility.command.tool.tool_train import ToolTrain
 from utility.entity.player import Player
 from utility.entity.CPU import CPU
 from utility.entity.combat import Combat
+from utility.entity.character import CharacterGetter, CharacterExperience
 
 
 class CommandTrain(commands.Cog):
@@ -22,6 +26,9 @@ class CommandTrain(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.tool   = ToolTrain(self.client)
+
+        # Rewards
+        self.reward_experience = 45
 
     @commands.check(CommandChecker.game_ready)
     @commands.check(CommandChecker.register)
@@ -44,6 +51,37 @@ class CommandTrain(commands.Cog):
 
         # Run the combat
         winner = await combat.run()
+        
+        # Display the winner name
+        message = f"🏆 **{winner.name}** has won the fight ! "
+
+        # If the winner is a player, grant xp to his team
+        if winner is player:
+            exp_message   = ""
+            unique_ids    = player.combat.unique_id_team
+            exp_manager   = CharacterExperience(self.client)
+            charar_getter = CharacterGetter()
+
+            for character in unique_ids:
+                await asyncio.sleep(0)
+
+                # Generate an exp amount
+                exp_amount = random.randint(int(self.reward_experience * 0.75), self.reward_experience)
+                
+                # Add exp to the character and check if it leveled up
+                new_level = await exp_manager.add_experience(character, exp_amount)
+
+                # Get the character's data
+                character_data = await charar_getter.get_from_unique(self.client, self.client.database, character)
+                                        
+                # If the character had leveled up
+                if new_level is not None:
+                    exp_message += f":star: **{character_data.name}**{character_data.type.icon} has won **{exp_amount}**xp :star: and reached the level **{new_level:,}**\n"
+                
+                else:
+                    exp_message += f":star: **{character_data.name}**{character_data.type.icon} has won **{exp_amount}**xp :star:\n"
+        
+        await context.send(exp_message)
 
 
 def setup(client):
