6724d8e26ee03c9b42f89da7e22cb3cf4bc8a8c4
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-08-04 19:44:48 +0100

    Add mission's cache setter call

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Project start : 09/03/20
 
 Author : DrLarck
 
-Last update : 03/08/20 by DrLarck
+Last update : 04/08/20 by DrLarck
 """
 
 import logging
@@ -22,6 +22,7 @@ from utility.logger.command_logger import CommandLogger
 from utility.command.loader import CommandLoader
 from utility.entity.character import CharacterGetter
 from utility.entity.banner import BannerGetter
+from utility.entity.mission import MissionGetter
 
 
 class Main:
@@ -62,6 +63,7 @@ class Main:
         # Filling up the cache
         client.loop.run_until_complete(CharacterGetter().set_cache(client))
         client.loop.run_until_complete(BannerGetter().set_cache(client))
+        client.loop.run_until_complete(MissionGetter().set_cache(client))
 
         # Loading the commands
         client.loop.run_until_complete(CommandLoader(client).load_commands())
