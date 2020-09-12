4d66988530183cba50a1037b9354f7e2b6d02f89
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-09-12 21:21:21 +0200

    Add trade command

---
+++ command/trade.py
@@ -0,0 +1,38 @@
+"""Trade command
+
+--
+
+@author DrLarck
+
+@update 12/09/20 by DrLarck"""
+
+from discord.ext import commands
+
+# tool
+from utility.command.tool import ToolTrade
+
+# util
+from utility.command.checker import CommandChecker
+from utility.entity.player import Player
+
+
+class CommandTrade(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+        self.tool   = ToolTrade(self.client)
+
+    @commands.check(CommandChecker.game_ready)
+    @commands.check(CommandChecker.register)
+    @commands.command()
+    async def trade(self, context, target: discord.Member):
+        """Allows player to trade items"""
+
+        player_a = Player(context, self.client, ctx.message.author)
+        player_b = Player(context, self.client, target)
+
+        await self.tool.trade(context, player_a, player_b)
+        
+
+def setup(client):
+    client.add_cog(CommandTrade(client))
