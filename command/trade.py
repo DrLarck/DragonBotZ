ca0209ca17c38097ac351aee7902c072070783b3
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-09-13 15:13:18 +0200

    Add checks

--- command/trade.py
+++ command/trade.py
@@ -11,6 +11,7 @@ from discord.ext import commands
 
 # tool
 from utility.command.tool.tool_trade import ToolTrade
+from utility.command.tool.tool_trade import TradeGetter
 
 # util
 from utility.command.checker import CommandChecker
@@ -22,9 +23,13 @@ class CommandTrade(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.tool   = ToolTrade(self.client)
+        self.getter = TradeGetter()
 
     @commands.check(CommandChecker.game_ready)
     @commands.check(CommandChecker.register)
+    @commands.check(CommandChecker.not_fighting)
+    @commands.check(CommandChecker.no_dm)
+    @commands.check(CommandChecker.not_trading)
     @commands.command()
     async def trade(self, context, target: discord.Member):
         """Allows player to trade items"""
@@ -33,6 +38,13 @@ class CommandTrade(commands.Cog):
 
         player_a = Player(context, self.client, context.message.author)
         player_b = Player(context, self.client, target)
+
+        # Check if the player_b is in trade
+        player_b_trading = await self.getter.is_trading(player_b)
+
+        # If the player b is in trade, just return
+        if player_b_trading:
+            return
 
         if player_a.id == player_b.id:
             await context.send(":x: You can't trade with yourself")
