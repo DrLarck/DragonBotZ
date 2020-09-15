a75bccbfb76b0ed1d8a98de64b002eda208cf475
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-09-15 11:08:50 +0200

    Add path to the main file

--- launcher.sh
+++ launcher.sh
@@ -5,6 +5,7 @@ tmux new-session -d -s dragonbotz # Create the sesssion named dragonbotz
 
 # Dragon Bot Z managment
 # Execute commands in the tmux session
+tmux send-keys -t dragonbotz "cd app/bot/discordballz/discordballz-origins/" C-m # Open the game directory
 tmux send-keys -t dragonbotz "echo \"Starting Dragon Bot Z : Origins ...\"" C-m
 tmux send-keys -t dragonbotz "pkill -F dragonbotz.pid" C-m  # Kill the process
 tmux send-keys -t dragonbotz "python3.6 -B main.py &"  C-m  # Starts the bot
