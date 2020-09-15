8d7eb3096ab9e419f283fac4eba6b5b21edea17f
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-09-15 10:49:18 +0200

    Add tmux-session managment

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# Tmux session managment
+tmux kill-session -t dragonbotz   # Kill the dragonbotz session
+tmux new-session -d -s dragonbotz # Create the sesssion named dragonbotz
+
+# Dragon Bot Z managment
 echo "Starting Dragon Bot Z : Origins ..."
-pkill -F dragonbotz.pid
-python3.6 -B main.py &
-echo $! > dragonbotz.pid
+pkill -F dragonbotz.pid  # Kill the process
+python3.6 -B main.py &   # Starts the bot
+echo $! > dragonbotz.pid # Stores the process pid
