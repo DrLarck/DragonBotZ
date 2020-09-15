79466ad7806543194eddc2f11f0ceb77ab570abe
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-09-15 10:59:43 +0200

    Send commands to the tmux session

--- launcher.sh
+++ launcher.sh
@@ -4,7 +4,8 @@ tmux kill-session -t dragonbotz   # Kill the dragonbotz session
 tmux new-session -d -s dragonbotz # Create the sesssion named dragonbotz
 
 # Dragon Bot Z managment
-echo "Starting Dragon Bot Z : Origins ..."
-pkill -F dragonbotz.pid  # Kill the process
-python3.6 -B main.py &   # Starts the bot
-echo $! > dragonbotz.pid # Stores the process pid
+# Execute commands in the tmux session
+tmux send-keys -t dragonbotz "echo \"Starting Dragon Bot Z : Origins ...\""
+tmux send-keys -t dragonbotz "pkill -F dragonbotz.pid"  # Kill the process
+tmux send-keys -t dragonbotz "python3.6 -B main.py &"   # Starts the bot
+tmux send-keys -t dragonbotz "echo $! > dragonbotz.pid" # Stores the process pid
