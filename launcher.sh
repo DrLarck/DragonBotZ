922fa2630ef8456a70704dd9b0520708d3ec2987
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2020-09-15 11:02:57 +0200

    Add C-m key to run commands

--- launcher.sh
+++ launcher.sh
@@ -5,7 +5,7 @@ tmux new-session -d -s dragonbotz # Create the sesssion named dragonbotz
 
 # Dragon Bot Z managment
 # Execute commands in the tmux session
-tmux send-keys -t dragonbotz "echo \"Starting Dragon Bot Z : Origins ...\""
-tmux send-keys -t dragonbotz "pkill -F dragonbotz.pid"  # Kill the process
-tmux send-keys -t dragonbotz "python3.6 -B main.py &"   # Starts the bot
-tmux send-keys -t dragonbotz "echo $! > dragonbotz.pid" # Stores the process pid
+tmux send-keys -t dragonbotz "echo \"Starting Dragon Bot Z : Origins ...\"" C-m
+tmux send-keys -t dragonbotz "pkill -F dragonbotz.pid" C-m  # Kill the process
+tmux send-keys -t dragonbotz "python3.6 -B main.py &"  C-m  # Starts the bot
+tmux send-keys -t dragonbotz "echo $! > dragonbotz.pid" C-m # Stores the process pid
