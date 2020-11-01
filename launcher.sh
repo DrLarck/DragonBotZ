681a1245cafae18000bea5562014d82dfe381bf4
DrLarck <lahcene.belhadi@gmail.com>
2020-11-02 00:01:36 +0100

    Rename main and add new launcher

--- launcher.sh
+++ launcher.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Tmux session managment
-tmux kill-session -t dragonbotz   # Kill the dragonbotz session
-tmux new-session -d -s dragonbotz # Create the sesssion named dragonbotz
 
-# Dragon Bot Z managment
-# Execute commands in the tmux session
-tmux send-keys -t dragonbotz "cd app/bot/discordballz/discordballz-origins/" C-m # Open the game directory
-tmux send-keys -t dragonbotz "echo \"Starting Dragon Bot Z : Origins ...\"" C-m
-tmux send-keys -t dragonbotz "pkill -F dragonbotz.pid" C-m  # Kill the process
-tmux send-keys -t dragonbotz "python3.6 -B main.py &"  C-m  # Starts the bot
-tmux send-keys -t dragonbotz "echo $! > dragonbotz.pid" C-m # Stores the process pid
+# Manage the tmux sessions
+tmux kill-session -t dragonbotz
+tmux new-session -d -s dragonbotz
+
+# Kill old instances
+pkill -f dragonbotz.py
+
+# Start the game into the tmux session
+tmux send-keys -t dragonbotz "cd /home/app/bot/dragonbotz/discordballz-origins/" C-m
+tmux send-keys -t dragonbotz "python3 -B dragonbotz.py" C-m
