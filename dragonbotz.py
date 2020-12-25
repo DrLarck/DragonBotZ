68041215707cb2ce3c1da30a89d10a4e7528a10f
DrLarck <lahcene.belhadi@gmail.com>
2020-12-25 19:09:02 +0100

    update version to v2.1.0.255

--- dragonbotz.py
+++ dragonbotz.py
@@ -30,7 +30,7 @@ class Main:
 
     def __init__(self):
         self.__TOKEN = os.environ["dev_dbz_token"]
-        self.__version = "2.1.0.254"
+        self.__version = "2.1.0.255"
         self.__phase = ["ALPHA", "BETA", "RELEASE", "STABLE"]
 
         self.__prefix = ["d!", "D!", "db", "Db"]
