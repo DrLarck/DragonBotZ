661b0d00c36b0325582d6abdaa9a86dd09e4cde5
DrLarck <lahcene.belhadi@gmail.com>
2020-12-25 20:06:50 +0100

    add top.gg webhook

---
+++ utility/event/top_gg.py
@@ -0,0 +1,47 @@
+"""Top.gg integration https://github.com/top-gg/python-sdk
+
+- Contains integrated webhook
+
+- Auto server count post
+
+@author DrLarck
+
+@update 25/12/20 by DrLarck"""
+
+import os
+
+from discord.ext import commands
+import dbl
+
+
+class TopGgWebhook(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+        self.token = os.environ["dev_dbz_dbl_token"]
+        self.dbl = dbl.DBLClient(self.client, self.token, 
+                                 webhook_path='/dblwebhook', webhook_port=8152)
+    
+    @commands.Cog.listener()
+    async def on_dbl_vote(self, data):
+        """Triggered when a vote is cast on top.gg
+
+        @param data - ` dict`
+
+        @return - `None`"""
+
+        print(data)
+
+    @commands.Cog.listener()
+    async def on_dbl_test(self, data):
+        """Triggered when a test is cast
+
+        @param data - `dict` 
+
+        @return - `None`"""
+
+        print("test", data)
+
+
+def setup(client):
+    client.add_cog(TopGgWebhook(client))
