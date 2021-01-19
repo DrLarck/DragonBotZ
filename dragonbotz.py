52bb81e6eded27f18bb330de4294417dc2d8033a
Belhadi Lahcène <33419226+DrLarck@users.noreply.github.com>
2021-01-19 17:02:24 +0100

    update version to v2.1.0.267rc - RELEASE CANDIDATE

--- dragonbotz.py
+++ dragonbotz.py
@@ -7,7 +7,7 @@ Project start : 09/03/20
 
 Author : DrLarck
 
-Last update : 25/12/20 by DrLarck
+Last update : 19/01/21 by DrLarck
 """
 
 import logging
@@ -30,7 +30,7 @@ class Main:
 
     def __init__(self):
         self.__TOKEN = os.environ["dev_dbz_token"]
-        self.__version = "2.1.0.266"
+        self.__version = "2.1.0.267rc"
         self.__phase = ["ALPHA", "BETA", "RELEASE", "STABLE"]
 
         self.__prefix = ["d!", "D!", "db", "Db"]
@@ -45,7 +45,7 @@ class Main:
         """
 
         # Init
-        activity = discord.Game(name=f"d!help | v{self.__version} - {self.__phase[0]}")
+        activity = discord.Game(name=f"d!help | v{self.__version} - {self.__phase[2]}")
 
         client = commands.AutoShardedBot(command_prefix=self.__prefix, help_command=None,
                               activity=activity)
