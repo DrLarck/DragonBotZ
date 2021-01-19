3af25e8bcd8c71e4e617bc5da987c50c5bbf59ea
DrLarck <lahcene.belhadi@gmail.com>
2021-01-19 17:20:16 +0100

    send rewards and message to voter

--- utility/event/top_gg.py
+++ utility/event/top_gg.py
@@ -6,12 +6,15 @@
 
 @author DrLarck
 
-@update 18/01/20 by DrLarck"""
+@update 19/01/20 by DrLarck"""
 
 import os
+import dbl
+import discord
 
 from discord.ext import commands
-import dbl
+from utility.entity.player import Player
+from utility.graphic.icon import GameIcon
 
 
 class TopGgWebhook(commands.Cog):
@@ -34,18 +37,49 @@ class TopGgWebhook(commands.Cog):
 
         @return - `None`"""
 
-        print(data)
+        weekend = data["isWeekend"]
+        icon = GameIcon()
+        user_id = int(data["user"])
 
-    @commands.Cog.listener()
-    async def on_dbl_test(self, data):
-        """Triggered when a test is cast
+        # Rewards
+        reward_ds = 25
+        reward_zenis = 1000
+        reward_power = 100
 
-        @param data - `dict` 
+        # If it's weekend, rewards are doubled
+        if weekend:
+            reward_ds *= 2
+            reward_zenis *= 2
+            reward_power *= 2
 
-        @return - `None`"""
+        player = await Player(None, self.client, None).get_player_from_id(
+            user_id
+        )
 
-        print("test", data)
+        if player is not None:
+            # Sends rewards to the player
+            await player.resource.add_dragonstone(reward_ds)
+            await player.resource.add_zeni(reward_zenis)
+            await player.experience.add_power(reward_power)
 
+            # Send dm
+            user = await self.client.get_user(user_id)
+
+            try:
+                await user.send(f"Thanks for voting ! Here are **{reward_ds}**{icon.dragonstone}, as well as **{reward_zenis}**{icon.zenis} and **{reward_power}**:star:")
+            
+            except discord.Forbidden:
+                print(f"Failed to send reward message to {user_id} : Permission denied")
+                pass
+
+            except discord.HTTPException:
+                print(f"Failed to send reward message to {user_id}")
+                pass
+                
+            else:
+                pass
+        
+        return
 
 def setup(client):
     client.add_cog(TopGgWebhook(client))
